80d0243e5b957d7d61b25843438b80df3e8fc202
DayDev <sabhyasoni20@gmail.com>
2022-05-28 00:02:43 +0530

    Projects

--- src/sab/oop/FourthProject.java
+++ src/sab/oop/FourthProject.java
@@ -5,16 +5,26 @@ import java.util.Scanner;
 public class FourthProject {
 
 	public static void main(String[] args) {
-		float p,y,i,si,a;
+		float p,y,i,si,a,A,ci;
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter Principal Amount, no. of years and the rate of interest");
 		p=scan.nextFloat();
 		y=scan.nextFloat();
 		i=scan.nextFloat();
 		si=p*y*i/100;
-		System.out.println("Simple Interest = "+si);
+		System.out.println("Simple Interest = Rs."+si);
 		a=p*1+si;
-		System.out.println("Total accured amount = $"+a);
+		System.out.println("Total accured amount = Rs."+a);
+		A=p*(1+i/100)*(1+i/100);
+		if(y==2) {
+		A=p*(1+i/100)*(1+i/100);
+		}
+		if(y==3) {
+			A=p*(1+i/100)*(1+i/100)*(1+i/100);
+		}
+		System.out.println("Amount earned = Rs."+A);
+		ci=A-p;
+		System.out.println("Compound Interest = Rs."+ci);
 		
 		
 
--- src/sab/oop/NinthProject.java
+++ src/sab/oop/NinthProject.java
@@ -1,9 +1,35 @@
 package sab.oop;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class NinthProject {
 
 	public static void main(String[] args) {
-		System.out.println("This is the ninth file.");
+		Scanner scan = new Scanner(System.in);
+		String a,b,c;
+		
+		System.out.println("Enter first Letter");
+		a=scan.next();
+		System.out.println("Enter second Letter");
+		b=scan.next();
+		System.out.println("Enter third Letter");
+		c=scan.next();
+		
+		 // Creating an array list
+        ArrayList<String> Sentence = new ArrayList<>();
+
+        // Add elements
+        Sentence.add(a);
+        Sentence.add(b);
+        Sentence.add(c);
+        System.out.println("Unsorted ArrayList: " + Sentence);
+
+        // Using the sort() method
+        Collections.sort(Sentence);
+        System.out.println("Sorted ArrayList: " + Sentence);
+
 
 	}
 
