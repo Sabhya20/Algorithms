8fdfa71bfaaa168275df0ee930c77ced848040e9
DayDev <sabhyasoni20@gmail.com>
2022-05-28 11:03:55 +0530

    Fixed Snetence problem

--- src/sab/oop/NinthProject.java
+++ src/sab/oop/NinthProject.java
@@ -7,26 +7,28 @@ import java.util.Scanner;
 public class NinthProject {
 
 	public static void main(String[] args) {
+		//Initilaize Variables
 		Scanner scan = new Scanner(System.in);
 		String a,b,c;
 		
+		//Get input from user 
 		System.out.println("Enter first Letter");
-		a=scan.next();
+		a=scan.nextLine();
 		System.out.println("Enter second Letter");
-		b=scan.next();
+		b=scan.nextLine();
 		System.out.println("Enter third Letter");
-		c=scan.next();
+		c=scan.nextLine();
 		
 		 // Creating an array list
         ArrayList<String> Sentence = new ArrayList<>();
 
-        // Add elements
+        // Add the elements from the recorded input
         Sentence.add(a);
         Sentence.add(b);
         Sentence.add(c);
         System.out.println("Unsorted ArrayList: " + Sentence);
 
-        // Using the sort() method
+        // Using the sort() method we can sort our sentence
         Collections.sort(Sentence);
         System.out.println("Sorted ArrayList: " + Sentence);
 
